cf983ef046ab0090ba9d23d32ee3817d9a6d1299
Will Acton <will@willacton.com>
2018-03-31 15:39:00 -0700

    basic socket server & beginning of protocol

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [me.raynes/conch "0.8.0"]]
+                 [me.raynes/conch "0.8.0"]
+                 [mount "0.1.12"]]
   :main ^:skip-aot mim.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mim/core.clj
+++ src/mim/core.clj
@@ -1,11 +1,59 @@
 (ns mim.core
-  (:require [me.raynes.conch.low-level :as sh])
+  (:require [me.raynes.conch.low-level :as sh]
+            [mount.core :as mount :refer [defstate]]
+            [clojure.core.server :as socket]
+            [clojure.edn :as edn]
+            [clojure.string :as str])
   (:gen-class))
 
 (def tasks {:build "echo hello"})
 
 
+(comment (def p (sh/proc "bash" "-c" (get-in tasks [:build])))
+
+         (def exit (future (sh/exit-code p)))
+
+         (sh/stream-to-out p :out)
+
+         (sh/destroy p))
+
+
+(defmulti do-task! class)
+
+(defmethod do-task! java.lang.String
+  [cmd]
+  (let [p (sh/proc "bash" "-c" cmd)]
+            (sh/stream-to-out p :out)
+            (sh/destroy p)))
+
+;; (defn prompt []
+;;   (print "=> ")
+;;   (flush)
+;;   (read-line))
+
+(defn parse-payload
+  "Parses a string payload into EDN"
+  [payload]
+  (let [parsed (clojure.edn/read-string payload)
+        ;; get args as a vector
+        args (str/split (:args parsed) #" ")]
+    (assoc parsed :args args)))
+
+(defn server-handler [& args]
+  (let [payload (parse-payload (read-line))]
+    (println payload)
+    (println (type (:cwd payload)))))
+
+(defstate socket-server
+  :start (socket/start-server {:port 1234
+                               :name "mim-socket"
+                               :accept 'mim.core/server-handler})
+  :stop (socket/stop-server "mim-socket"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (mount/start))
+
+(comment (do-task! "cat")
+         (mount/stop))
